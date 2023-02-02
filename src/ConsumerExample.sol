171163b077b139349024f0af0db1e8bfe3595426
SolidityDrone <104315978+SolidityDrone@users.noreply.github.com>
2023-02-02 14:15:04 +0100

    Formatted

--- src/ConsumerExample.sol
+++ src/ConsumerExample.sol
@@ -1,7 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-
+    /// This is an example do your own implementation. We pass the swap addres in constructor to facilitate
+    /// test and comprehension, you can initialize contract your own way
+    
  
 interface ISwapMaticToLink{
     function swap() external payable returns (uint256);
@@ -18,12 +20,11 @@ contract ConsumerContract  {
     }
     
     //Assume this address to be funded;
+    //this function MAY be called multiple from another function to allow swapping more than 100k matic
     function callSwap(uint256 payableAmount) public returns (uint256){
         uint256 amountSwapped = ISwapMaticToLink(_swap).swap{value: payableAmount}();
         return amountSwapped;
     }
-
-    
 }
 
 
